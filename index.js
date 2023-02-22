09686ce8caa98b611aa11ee434b9cadb8d11be87
matt roche <matt.roche@gmail.com>
2023-02-22 17:16:36 -0600

    complete functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+    return 'This Saturday, I want to roller-skate!';}
+    else {
+         return (`This Saturday, I want to ${activity}!`);
+    }
+}
+
+saturdayFun()
+
+saturdayFun('bathe my dog')
+
+const mondayWork = function(act) {
+    if (act === undefined) {
+        return 'This Monday, I will go to the office.';}
+        else {
+            return (`This Monday, I will ${act}.`);
+    }
+ }
+
+ let wrapAdjective = function(style="*") {
+    return function(adj="special") {
+      return `You are ${style}${adj}${style}!`
+    }
+  }
